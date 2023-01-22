2b73a700172baea119f4a439ae2eea5da0f47f35
sumit <sumitsaurav121@gmail.com>
2023-01-22 20:28:22 +0530

    updated the ssl configuration

--- src/main/java/com/gehc/uls/subscriber/entity/ConnectionFactoryBuilder.java
+++ src/main/java/com/gehc/uls/subscriber/entity/ConnectionFactoryBuilder.java
@@ -6,20 +6,28 @@ import java.security.KeyManagementException;
 import java.security.NoSuchAlgorithmException;
 
 public class ConnectionFactoryBuilder {
-    public static final ConnectionFactory connectionFactory = new ConnectionFactory();
+    private static final ConnectionFactory connectionFactory = new ConnectionFactory();
+    private static final int SECURE_PROTOCOL = 5671;
 
 
     public static ConnectionFactory getInstance() {
         return connectionFactory;
     }
 
-    public static void setDetails(String username, String password, String virtualHost, String hostName, int portNumber) {
+    public static void setDetails(String username, String password, String virtualHost, String hostName, int portNumber, String useSsl) {
         connectionFactory.setUsername(username);
         connectionFactory.setPassword(password);
-        connectionFactory.setVirtualHost(virtualHost);
+        // connectionFactory.setVirtualHost(virtualHost);
         connectionFactory.setHost(hostName);
         connectionFactory.setPort(portNumber);
-        if (portNumber == 5671) {
+
+        if (virtualHost != null && !virtualHost.isEmpty()) {
+            connectionFactory.setVirtualHost(virtualHost);
+        } else {
+            connectionFactory.setVirtualHost("/");
+        }
+
+        if (useSsl != null && !useSsl.isEmpty() && useSsl.equalsIgnoreCase("true")) {
             try {
                 connectionFactory.useSslProtocol();
             } catch (NoSuchAlgorithmException | KeyManagementException e) {
